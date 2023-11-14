f309c593f0a86c3163d346a5e5b96622b79a0fdf
lythanhlongdev <lythanhlongfmc@gmail.com>
2023-11-14 19:11:35 +0700

    update fe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent,
+    DetailProductComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap:[HomeComponent] // fisrt run
+  bootstrap:[
+    // HomeComponent
+    // OrderComponent
+    // OrderConfirmComponent
+    // LoginComponent
+    // RegisterComponent
+    DetailProductComponent
+  ] // fisrt run
 })
 export class AppModule { }
